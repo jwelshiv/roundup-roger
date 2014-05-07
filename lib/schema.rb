6034e7bb4579762f57d81a57a0f17b90663d92d8
Jenn Cooper <cooper.jennl@gmail.com>
2014-05-06 19:16:02 -0700

    comment out schema

--- lib/schema.rb
+++ lib/schema.rb
@@ -2,11 +2,11 @@ require 'sequel'
 
 DB = Sequel.connect(ENV['DATABASE_URL'])
 
-DB.create_table :emails do
-  primary_key :id
-  String :payload
-  String :from_email
-  String :from_name
-  String :body
-  Time :created_at
-end
+# DB.create_table :emails do
+#   primary_key :id
+#   String :payload
+#   String :from_email
+#   String :from_name
+#   String :body
+#   Time :created_at
+# end
