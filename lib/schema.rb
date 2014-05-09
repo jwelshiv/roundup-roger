d4e53a231347d24e609785a7bab5acb44592df71
Jenn Cooper <cooper.jennl@gmail.com>
2014-05-09 11:13:45 -0700

    don't try to create table if it already exists

--- lib/schema.rb
+++ lib/schema.rb
@@ -2,11 +2,13 @@ require 'sequel'
 
 DB = Sequel.connect(ENV['DATABASE_URL'])
 
-# DB.create_table :emails do
-#   primary_key :id
-#   String :payload
-#   String :from_email
-#   String :from_name
-#   String :body
-#   Time :created_at
-# end
+if not DB[:emails].exists
+  DB.create_table :emails do
+    primary_key :id
+    String :payload
+    String :from_email
+    String :from_name
+    String :body
+    Time :created_at
+  end
+end
