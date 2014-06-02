9ddc702f3777e347d389ed21c07257fca54dcf47
Jenn Cooper <cooper.jennl@gmail.com>
2014-06-01 22:27:14 -0700

    make sure regex doesn't strip regular sentences starting with 'On', and fix specs to run properly on weekends

--- spec/send_reminder_emails_spec.rb
+++ spec/send_reminder_emails_spec.rb
@@ -3,7 +3,7 @@ require 'roundup_roger'
 describe SendReminderEmails do
   it "works" do
     expect(SendOutboundEmail).to receive(:call)
-    SendReminderEmails.call
+    SendReminderEmails.call(Time.new(2014, 05, 02, 7))
   end
 
   it "doesn't send on Saturday" do
--- spec/send_roundup_email_spec.rb
+++ spec/send_roundup_email_spec.rb
@@ -4,7 +4,7 @@ describe SendRoundupEmail do
   it "sends when there are emails to roundup" do
     Email.create(body: "hi there")
     expect(SendOutboundEmail).to receive :call
-    SendRoundupEmail.call(Time.now)
+    SendRoundupEmail.call(Time.new(2014, 05, 02, 7))
   end
 
   it "doesn't send when there are no emails in the last 24 hours" do
